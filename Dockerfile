00782326122ef3c5fffeddcd6b2621673e493891
Lukas Fittl <lukas@fittl.com>
2015-10-14 04:22:47 -0700

    Change Docker build to be based on Alpine linux.

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,21 @@
-FROM phusion/baseimage:0.9.16
+FROM gliderlabs/alpine:3.2
+MAINTAINER team@pganalyze.com
 
-RUN groupadd -r pganalyze && useradd -r -g pganalyze pganalyze
+RUN apk add -U python curl ca-certificates rsyslog
+
+RUN curl -o /usr/local/bin/gosu -sSL "https://github.com/tianon/gosu/releases/download/1.6/gosu-amd64"
+RUN chmod +x /usr/local/bin/gosu
+
+RUN adduser -D pganalyze pganalyze
 ENV HOME_DIR /home/pganalyze
-
-RUN apt-get update && apt-get install -y python
 
 ADD . $HOME_DIR
 
 RUN chown pganalyze:pganalyze -R $HOME_DIR
-
-RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 COPY docker-entrypoint.sh /
 RUN chmod +x /docker-entrypoint.sh
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
-CMD ["/sbin/my_init"]
+CMD ["collector"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+
 set -e
+
+if [ "$1" != 'collector' ]; then
+  exec "$@"
+fi
 
 HOME_DIR=/home/pganalyze
 CONFIG_FILE=$HOME_DIR/.pganalyze_collector.conf
@@ -9,28 +14,34 @@ if [ -z "$PGA_API_KEY" ]; then
   exit 1
 fi
 
-if [ -z "$DB_NAME" ]; then
-  echo "Error: You need to set DB_NAME to your database name"
-  exit 1
-fi
-
 echo """
 [pganalyze]
 api_key: $PGA_API_KEY
-db_name: $DB_NAME
-db_host: ${DB_HOST:-db}
-db_port: ${DB_PORT:-5432}""" > $CONFIG_FILE
-
-if [ ! -z "$DB_USERNAME" ]; then
-  echo "db_username: $DB_USERNAME" >> $CONFIG_FILE
-fi
-
-if [ ! -z "$DB_PASSWORD" ]; then
-  echo "db_password: $DB_PASSWORD" >> $CONFIG_FILE
-fi
+"""  > $CONFIG_FILE
 
 if [ ! -z "$PGA_API_URL" ]; then
   echo "api_url: $PGA_API_URL" >> $CONFIG_FILE
+fi
+
+if [ ! -z "$DB_URL" ]; then
+  echo "db_url: $DB_URL" >> $CONFIG_FILE
+else
+  if [ -z "$DB_NAME" ]; then
+    echo "Error: You need to set DB_NAME to your database name"
+    exit 1
+  fi
+
+  echo "db_name: $DB_NAME" >> $CONFIG_FILE
+  echo "db_host: ${DB_HOST:-db}" >> $CONFIG_FILE
+  echo "db_port: ${DB_PORT:-5432}" >> $CONFIG_FILE
+
+  if [ ! -z "$DB_USERNAME" ]; then
+    echo "db_username: $DB_USERNAME" >> $CONFIG_FILE
+  fi
+
+  if [ ! -z "$DB_PASSWORD" ]; then
+    echo "db_password: $DB_PASSWORD" >> $CONFIG_FILE
+  fi
 fi
 
 chown pganalyze:pganalyze $CONFIG_FILE
@@ -38,10 +49,17 @@ chmod go-rwx $CONFIG_FILE
 
 echo "Doing initial collector test run..."
 
-setuser pganalyze python $HOME_DIR/pganalyze-collector.py
+gosu pganalyze python $HOME_DIR/pganalyze-collector.py $OPTS
 
-echo "*/10 * * * * pganalyze /usr/bin/python /home/pganalyze/pganalyze-collector.py --cron 2>&1 | /usr/bin/logger -t collector" > /etc/cron.d/pganalyze
+rm /var/spool/cron/crontabs/root
+echo "*/10 * * * * /usr/bin/python /home/pganalyze/pganalyze-collector.py --cron $OPTS 2>&1 | /usr/bin/logger -t collector" > /var/spool/cron/crontabs/pganalyze
 
 echo "Good to go, collector will run every 10 minutes"
 
-exec "$@"
+# These automatically run in the background
+/sbin/syslogd
+/usr/sbin/crond
+
+touch /var/log/messages
+
+tail -f /var/log/messages
